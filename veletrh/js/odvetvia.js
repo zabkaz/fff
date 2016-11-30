0f798c4a7c4e1329850ffb8c01d8697807dd6b0f
zabkaz <zuzkazabka@gmail.com>
2016-11-30 18:06:36 +0100

    profil

---
+++ veletrh/js/odvetvia.js
@@ -0,0 +1,17 @@
+function add_interest(name, id){    
+    $.ajax({    
+    type: "POST",  
+    url: "add_interest.php",
+    data: "name=" + name + "& id=" + id,
+    success: function(data){          
+        if(data.localeCompare('success') === 0){
+          $('.'+name).toggleClass('active')
+        }else{
+          alert('fail');
+        }         
+    },
+    error: function(XMLHttpRequest, textStatus, errorThrown) { 
+        alert("Status: " + textStatus + "|| Error: " + errorThrown);
+    }       
+  }); 
+}
