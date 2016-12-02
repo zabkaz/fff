bd7f9d69446287872b728cd97f95c2e434b9a85b
zabkaz <zuzkazabka@gmail.com>
2016-12-02 22:02:58 +0100

    login

---
+++ veletrh/probe_login_vyst.php
@@ -0,0 +1,16 @@
+<?php 
+    $login = trim($_POST['login']);
+    $pass = trim($_POST['pass']);
+
+    $connection = new mysqli('mysql14.hostmaster.sk', 'ws506600', 'weplD45Qx_', 'ws506608db');
+
+    $sql = "SELECT * FROM login_vyst WHERE username='$login' AND password='$pass'";
+    $result = $connection->query($sql);
+    $rows = $result->num_rows;
+    if ($rows == 1) {
+        echo 'success';
+    } else {
+        echo 'failed';
+    }
+    $connection->close();
+?>
