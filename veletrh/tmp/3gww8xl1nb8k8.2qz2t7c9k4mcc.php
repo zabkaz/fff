9108e7af39d98e0bb7868ef90246c1db0c67047c
zabkaz <zuzkazabka@gmail.com>
2016-12-20 10:56:13 +0100

    f3

--- veletrh/tmp/3gww8xl1nb8k8.2qz2t7c9k4mcc.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<head>
-<title><?php echo $ERROR['text']; ?></title>
-</head>
-<body>
-  <h1><?php echo $ERROR['text']; ?></h1>
-  <p>Error code: <?php echo $ERROR['code']; ?></p>
-</body>
-</html>
